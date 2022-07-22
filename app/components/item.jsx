48f658b7bd51b9b20ddfdbde3726715c4e9e831e
TheWisePigeon <zozozozeph@gmail.com>
2022-07-22 13:23:16 +0000

    added items listing page layout

--- app/components/item.jsx
+++ app/components/item.jsx
@@ -1,8 +1,14 @@
-export default function Item(props){
-    return(
+import { items } from "../__mocks__/items";
+
+export default function Item(props) {
+    return (
         <>
-            <div>
-                {props.model} <br />
+            <div class="w-fit flex-col text-lg font-bold font-mono">
+                <img src={`${props.image}`} alt=""/>
+                    <div class="flex justify-between">
+                        <div>{props.brand} {props.model}</div>
+                        <div>{props.price}/Jour</div>
+                    </div>
             </div>
         </>
     )
--- app/components/itemList.jsx
+++ app/components/itemList.jsx
@@ -2,16 +2,20 @@ import { items } from "../__mocks__/items"
 import Item from "./item"
 export default function ItemList(){
     return (
-        <>
+        <div className=" p-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  ">
             {
                 items.map(
                     item=>{
                         return(
-                            <Item key={item.model} model={item.brand} />
+                            <Item key={item.model} model={item.model} brand={item.brand} image={item.image} price={item.price} />
                         )
                     }
                 )
             }
-        </>
+        </div>
     )
 }
+
+/**
+ *  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 
+ */
