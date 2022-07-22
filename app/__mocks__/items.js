d3141aad0592169b717631bb2ba2985180a5ee8c
TheWisePigeon <zozozozeph@gmail.com>
2022-07-22 20:48:27 +0000

    added mocks

---
+++ app/__mocks__/items.js
@@ -0,0 +1,38 @@
+export const items = [
+    {
+        model: 'jjkdsd',
+        brand: 'Audi',
+        image: 'https://picsum.photos/500/500',
+        price: '$100',
+    },
+    {
+        model: 'dsdsd',
+        brand: 'Mercedes',
+        image: 'https://picsum.photos/500/500',
+        price: '$200',
+    },
+    {
+        model: 'wewewe',
+        brand: 'Jeep',
+        image: 'https://picsum.photos/500/500',
+        price: '$300',
+    },
+    {
+        model: 'zxczxc',
+        brand: 'Lamborghini',
+        image: 'https://picsum.photos/500/500',
+        price: '$400',
+    },
+    {
+        model: 'vbnvbn',
+        brand: 'Honda',
+        image: 'https://picsum.photos/500/500',
+        price: '$500',
+    },
+    {
+        model: 'ghjghj',
+        brand: 'Mazda',
+        image: 'https://picsum.photos/500/500',
+        price: '$600',
+    },
+]
